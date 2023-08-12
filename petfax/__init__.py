0dac82c53b3aa8b012ee0ba4db397d974ff2a37c
Chris Wehner <chrisw7128@gmail.com>
2023-08-12 12:54:34 -0700

    another big commit

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = "postgresql://postgres:2023!@localhost:5434/petfax"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    from . import models
+
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
     @app.route("/")
     def index():
--- petfax/facts.py
+++ petfax/facts.py
@@ -1,14 +1,30 @@
 import json
-from flask import Blueprint, render_template
+
+from . import models
+from flask import Blueprint, render_template, request, redirect
 
 bp_facts = Blueprint("facts", __name__, url_prefix="/facts")
 
 
-@bp_facts.route("/")
+@bp_facts.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("facts.html")
+    if request.method == "POST":
+        print(request.form)
+        # return render_template("facts.html")
+        submitter = request.form["submitter"]
+        fact = request.form["fact"]
+
+        new_fact = models.Fact(submitter, fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+
+        return redirect("/facts")
+
+    rows = models.Fact.query.all()
+
+    return render_template("facts/index.html", facts=rows)
 
 
 @bp_facts.route("/new")
 def facts_new():
-    return render_template("new_facts.html")
+    return render_template("facts/new.html")
